b251f159c4c24707c437240d2417c9634a1c12f3
rthomasbray <rtb439@utulsa.edu>
2017-11-02 15:04:46 -0500

    re arranged some logic

--- teams/admin.py
+++ teams/admin.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Teamname
+from .models import Teamname, Category
 
 admin.site.register(Teamname)
+admin.site.register(Category)
--- teams/models.py
+++ teams/models.py
@@ -3,15 +3,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Teamname(models.Model):
-    teamname_text =models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.teamname_text
-
 class Category(models.Model):
-    team = models.ForeignKey(Teamname, on_delete=models.CASCADE)
+    #team = models.ForeignKey(Teamname, on_delete=models.CASCADE)
     category_text = models.CharField(max_length=200)
 
     def __str__(self):
         return self.category_text
+
+
+class Teamname(models.Model):
+    teamname_text = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,default="hello")
+
+    def __str__(self):
+        return self.teamname_text
