346d6b333e9c3a44e036a5a7c0e84f8cdc0a22d5
rthomasbray <rtb439@utulsa.edu>
2017-11-02 17:11:53 -0500

    current fully working version

--- teams/models.py
+++ teams/models.py
@@ -2,6 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib import admin
+
+
+
+
+
 
 class Category(models.Model):
     #team = models.ForeignKey(Teamname, on_delete=models.CASCADE)
@@ -10,10 +16,17 @@ class Category(models.Model):
     def __str__(self):
         return self.category_text
 
-
 class Teamname(models.Model):
     teamname_text = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE,default="hello")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.teamname_text
+
+
+class Teammember(models.Model):
+    teammember_name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.teammember_name
