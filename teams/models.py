19578980f021454b2a3b61597481d3e8204a8bad
rthomasbray <rtb439@utulsa.edu>
2017-11-05 10:31:36 -0600

    Nothing really new

--- teams/models.py
+++ teams/models.py
@@ -4,12 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib import admin
 from django import forms
+from django.core.validators import MaxValueValidator
+
 
 
 
 
 class ScoringCriteria(models.Model):
     criteriaName = models.CharField(max_length = 100)
+    score =  models.PositiveIntegerField(default=0, validators=[MaxValueValidator(10),])
 
     def __str__(self):
         return self.criteriaName
@@ -34,7 +37,7 @@ class Member(models.Model):
 class Team(models.Model):
     teamName = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    member = models.ManyToManyField(Member, blank = True)
+    member = models.ManyToManyField(Member, blank=True)
     score = models.IntegerField()
 
     def __str__(self):
