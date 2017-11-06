bcb75fb38633318c7a91d96a0771addabddd90de
rthomasbray <rtb439@utulsa.edu>
2017-11-05 20:01:50 -0600

    getting the repository to a base state

--- main/apps.py
+++ main/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class TeamsConfig(AppConfig):
-    name = 'teams'
+class MainConfig(AppConfig):
+    name = 'main'
--- main/views.py
+++ main/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to the teams pages")
+    return HttpResponse("Welcome to the main")
