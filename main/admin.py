6674ce25bfeab5e26786843733a09f4bb9a862e6
andrew carlson <awestonc@gmail.com>
2017-11-07 14:52:12 -0600

    Update branch

--- main/admin.py
+++ main/admin.py
@@ -2,28 +2,17 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from main.models import UserProfile
 
-from django import forms
+# Register your models here.
 
-from .models import Team, Category, Member, ScoringCriteria
+class ProfileAdmin(admin.ModelAdmin):
+    def username(self, instance):
+        return instance.user.username
 
+    list_display = ('user_type', username)
 
+#
+#
 
-
-class MemberAdmin(admin.ModelAdmin):
-    pass
-
-class TeamAdmin(admin.ModelAdmin):
-    pass
-
-class CategoryAdmin(admin.ModelAdmin):
-    pass
-
-class ScoringCriteriaAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(ScoringCriteria, ScoringCriteriaAdmin)
-admin.site.register(Member, MemberAdmin)
-admin.site.register(Team, TeamAdmin)
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile, ProfileAdmin)
--- main/models.py
+++ main/models.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib import admin
-from django import forms
-from django.core.validators import MaxValueValidator
-
-from django.db import models
 from  django.contrib.auth.models import User
+
+
+
+#  current landing is plce is good.
+#
+#  one off task - create a user profile for each of the existing users
+#  user django admin's save
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
@@ -15,38 +17,3 @@ class UserProfile(models.Model):
     REGISTRAR = 2
     USER_TYPE_CHOICES = (  (JUDGE, 'Judge'), (REGISTRAR, 'Registrar') )
     user_type = models.IntegerField(choices=USER_TYPE_CHOICES)
-
-
-
-class ScoringCriteria(models.Model):
-    criteriaName = models.CharField(max_length = 100)
-    score =  models.PositiveIntegerField(default=0, validators=[MaxValueValidator(10),])
-
-    def __str__(self):
-        return self.criteriaName
-
-
-class Category(models.Model):
-
-    category = models.CharField(max_length=200)
-    criteria = models.ManyToManyField(ScoringCriteria, blank = True)
-
-    def __str__(self):
-        return self.category
-
-
-class Member(models.Model):
-    memberName = models.CharField(max_length = 100)
-
-    def __str__(self):
-        return self.memberName
-
-
-class Team(models.Model):
-    teamName = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    member = models.ManyToManyField(Member, blank=True)
-    score = models.IntegerField()
-
-    def __str__(self):
-        return self.teamName
