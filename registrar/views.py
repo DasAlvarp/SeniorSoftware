8df9eaaa68ed66e8fb97c4541cfd8d487cdc1e38
Zimo Chai (Jerry) <zic337@utulsa.edu>
2017-11-07 13:09:46 +0000

    Set up first registrar app page

--- registrar/views.py
+++ registrar/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def registrar(request):
+    return HttpResponse("Hello, world. You're at the registrar index.")
