88efcdb6643fdb876ad42205ec0bdf6aaee9d65f
Zimo Chai (Jerry) <zic337@utulsa.edu>
2017-11-07 14:33:54 +0000

    Add more functions

--- registrar/urls.py
+++ registrar/urls.py
@@ -3,5 +3,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /registrar/
     url(r'^$', views.registrar, name='registrar'),
 ]
+
+'''
+    # ex: /registrar/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /registrar/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /registrar/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+'''
