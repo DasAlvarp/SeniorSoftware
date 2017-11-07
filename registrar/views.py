e27fa22f06b914e33c2665c70736b605226b6f12
Zimo Chai (Jerry) <zic337@utulsa.edu>
2017-11-07 22:32:55 +0000

    Make /registrar page accessable

--- registrar/views.py
+++ registrar/views.py
@@ -8,6 +8,8 @@ from .models import Question
 '''
 The index page should the place 
 '''
+
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
